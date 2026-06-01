169e367767a336d9e1ab02134150d0778f3c13e6
joaokotsugai <joaokotsugai@aluno.ifsp.edu.br>
2026-06-01 08:42:33 -0300

    adicionando endpoints

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,14 @@
+import { RequestHandler } from 'express'
+
+export const signIn: RequestHandler = async (req, res) => {
+    // Lógica para autenticar o usuário
+}
+
+export const signUp: RequestHandler = async (req, res) => {
+    // Lógica para registrar um novo usuário
+}
+
+export const validate: RequestHandler = async (req, res) => {
+    // Lógica para validar o token de autenticação
+}
+
